20d2d33a6e39f17712e0ad0f8f468c50f7dc7ce8
Joymalya Basu Roy <86624602+jbasuroy369@users.noreply.github.com>
2024-04-06 17:06:02 +0530

    Add files via upload

---
+++ Disable_PrintSpoolerService.ps1
@@ -0,0 +1,10 @@
+﻿$service = Get-Service -Name "Spooler"
+if ($service.Status -eq "Running") 
+{
+    Stop-Service -name "Spooler" -force
+    Set-Service -name "Spooler" -startupType "Disabled"
+} 
+else 
+{
+    Set-Service -name "Spooler" -startupType "Disabled"
+}
---
+++ Enable_PrintSpoolerService.ps1
@@ -0,0 +1,11 @@
+﻿$service = Get-Service -Name "Spooler"
+if ($service.Status -eq "Running") 
+{
+    Set-Service -name "Spooler" -startupType "Automatic"
+} 
+else 
+{
+    Set-Service -name "Spooler" -startupType "Automatic"
+    sleep-start 10
+    Set-Service -Name "Spooler" -Status "Running"
+}

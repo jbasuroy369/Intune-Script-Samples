77075232cdbf6f579cc4f4df4b6a0deae96a23bc
Joymalya Basu Roy <86624602+jbasuroy369@users.noreply.github.com>
2024-04-06 17:02:40 +0530

    Add files via upload

---
+++ Initiate-MDMSync.ps1
@@ -0,0 +1,9 @@
+﻿[Windows.Management.MdmSessionManager, Windows.Management, ContentType = WindowsRuntime] | Out-Null
+        $MDMSession = [Windows.Management.MdmSessionManager]::TryCreateSession()
+        $MDMSession.StartAsync() | Out-Null
+
+        $MDMSyncTimeout = 0
+        do {
+            Start-Sleep -Seconds 5
+            $MDMSyncTimeout += 1
+        }while (($MDMSession.State -ne "Completed") -and ($MDMSyncTimeout -lt 12))
